f904041d239dd727e64004637964c757c87f0f36
ujwala-max <ujwala@sigmavalue.co.in>
2026-08-29 16:06:52 +0530

    Update pricing navigation route

--- src/features/navigation/Header.jsx
+++ src/features/navigation/Header.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { FiArrowUpRight, FiChevronDown, FiMenu } from "react-icons/fi";
 import { CHAPTERS } from "../experience/data/chapters";
 
